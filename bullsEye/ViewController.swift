f00050a31034d45f31aa0eac3c5d7fe4d98b50a1
vattanac <vattanacsim99@gmail.com>
2018-08-31 12:29:30 +0700

    push

--- bullsEye/ViewController.swift
+++ bullsEye/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int=200
+    @IBOutlet weak var slider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,10 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMove(_ slider: UISlider){
+        print("slider is :\(slider.value)")
+        currentValue=lroundf(slider.value)
+    }
+    
     @IBAction func showAlert(){
        
 //        make alert controll(POPUP)
-        let alert = UIAlertController(title: "Hello world", message: "This is my first App!", preferredStyle: .alert)
+        let message = "The value of slider is:\(currentValue)"
+        let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
         let  action = UIAlertAction(title: "Awsome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
