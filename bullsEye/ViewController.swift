461d87e1326987ef2a384d8e86cb02e168bf8d83
vattanac <vattanacsim99@gmail.com>
2018-08-31 13:40:14 +0700

    push

--- bullsEye/ViewController.swift
+++ bullsEye/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var currentValue: Int=200
+    var currentValue: Int=0
+    var targetValue: Int=0
     @IBOutlet weak var slider: UISlider!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
+        targetValue = Int(arc4random_uniform(100)) + 1
     }
 
+    func startNewRound(){
+        targetValue=1+Int(arc4random_uniform(100))
+        currentValue=50
+        slider.value=Float(currentValue)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,11 +39,13 @@ class ViewController: UIViewController {
     @IBAction func showAlert(){
        
 //        make alert controll(POPUP)
-        let message = "The value of slider is:\(currentValue)"
+        let message = "The value of slider is:\(currentValue)\n" +
+        "target values is :\(targetValue)"
         let alert = UIAlertController(title: "Hello world", message: message, preferredStyle: .alert)
         let  action = UIAlertAction(title: "Awsome", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
 
 }
